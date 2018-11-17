968b33b2747091e2da7467777ee99325ba42ebdf
Cole Wippern <cgwippern@gmail.com>
2018-11-17 14:01:04 -0800

    streaming unmarshall

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-build:
-  dep check
-  go build
+build: build-proto
+	dep check
+	go build
+
+build-proto:
+	protoc -I ./proto ./proto/proxysql/proxysql.proto --go_out=plugins=grpc:proto
 
 test:
-  go test ./...
+	go test ./...
--- main.go
+++ main.go
@@ -1,3 +1,64 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+
+	"github.com/cvgw/proto-stream-exp/proto/proxysql"
+	"github.com/gogo/protobuf/proto"
+	log "github.com/sirupsen/logrus"
+)
+
+func newQueryDigestBinData(hostGroup int) []byte {
+	queryDigest := &proxysql.QueryDigest{
+		HostGroup: int64(hostGroup),
+	}
+
+	binData, err := proto.Marshal(queryDigest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return binData
+}
+
+func testUnmarshal(b []byte) proxysql.QueryDigest {
+	mQueryDigest := proxysql.QueryDigest{}
+	err := proto.Unmarshal(b, &mQueryDigest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info(mQueryDigest)
+	log.Infof("proto message size %d", proto.Size(&mQueryDigest))
+
+	return mQueryDigest
+}
+
+func main() {
+	binData := newQueryDigestBinData(99)
+
+	blob := make([]byte, 0)
+	buf := bytes.NewBuffer(blob)
+	buf.Write(binData)
+
+	log.Info(buf.Bytes())
+
+	testUnmarshal(buf.Bytes())
+
+	for i := 1; i < 11; i++ {
+		binData := newQueryDigestBinData(i)
+
+		buf.Write(binData)
+	}
+
+	log.Info(buf.Bytes())
+
+	b := buf.Bytes()
+	message := testUnmarshal(b)
+	msgSize := proto.Size(&message)
+	for len(b) > msgSize {
+		b = b[:len(b)-msgSize]
+		testUnmarshal(b)
+		msgSize = proto.Size(&message)
+	}
+}
