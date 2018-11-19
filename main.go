1613e058fffd6d0a3ca6ad0f4b65d27218f9b808
Cole Wippern <cgwippern@gmail.com>
2018-11-18 17:19:51 -0800

    unmarshal overwrites

--- main.go
+++ main.go
@@ -8,57 +8,35 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func newQueryDigestBinData(hostGroup int) []byte {
-	queryDigest := &proxysql.QueryDigest{
-		HostGroup: int64(hostGroup),
-	}
-
-	binData, err := proto.Marshal(queryDigest)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return binData
-}
-
-func testUnmarshal(b []byte) proxysql.QueryDigest {
-	mQueryDigest := proxysql.QueryDigest{}
-	err := proto.Unmarshal(b, &mQueryDigest)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Info(mQueryDigest)
-	log.Infof("proto message size %d", proto.Size(&mQueryDigest))
-
-	return mQueryDigest
-}
-
 func main() {
-	binData := newQueryDigestBinData(99)
+	queryDigestHg := &proxysql.QueryDigest{
+		HostGroup: int64(99),
+	}
+
+	binDataHg, err := proto.Marshal(queryDigestHg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryDigestD := &proxysql.QueryDigest{
+		Digest: "meow",
+	}
+
+	binDataD, err := proto.Marshal(queryDigestD)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	blob := make([]byte, 0)
 	buf := bytes.NewBuffer(blob)
-	buf.Write(binData)
 
+	buf.Write(binDataHg)
 	log.Info(buf.Bytes())
 
-	testUnmarshal(buf.Bytes())
-
-	for i := 1; i < 11; i++ {
-		binData := newQueryDigestBinData(i)
-
-		buf.Write(binData)
-	}
-
+	buf.Write(binDataD)
 	log.Info(buf.Bytes())
 
 	b := buf.Bytes()
 	message := testUnmarshal(b)
-	msgSize := proto.Size(&message)
-	for len(b) > msgSize {
-		b = b[:len(b)-msgSize]
-		testUnmarshal(b)
-		msgSize = proto.Size(&message)
-	}
+	log.Info(message)
 }
